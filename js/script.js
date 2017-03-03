76876a8687d1a9f498665d3f0ef4e3efb9f22c4a
chaohaiLuo <chaohai93@qq.com>
2017-03-03 12:20:28 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-function g(selector){
+function g(selector){//通用函数
     var method = selector.substr(0,1)=='.'?'getElementsByClassName':'getElementById';
-    return document[method](selector.substr(1)); //不明白这里为什么是document[method] 这样子就会自动转换成document.method了呢，那个 . 在哪里
+    return document[method](selector.substr(1)); 
 }
-function random( range ){
+function random( range ){//随机生成一个值，支持取值范围为图片张数
     var max = Math.max(range[0],range[1]);
     var min = Math.min(range[0],range[1]);
     var diff = max - min;
     var number = Math.ceil(Math.random()*diff+min);
-    //这里用的是ceil 会向上取整，所以比如random(1,3)会取得2，3。因此，如果想取得1到8的随机数，应写random(0,8)
+    //ceil 会向上取整
     return number;
 }
+//输出所有的图片数据
 var mydata = data;
 function addPhotos(){
     var template = g('#wrap').innerHTML;
     var html=[];
     var nav=[];
-    for(var s in mydata){
+    for(var s in mydata){//输出控制按钮，每一个按钮对应一张图片
         var _html = template
             .replace('{{index}}',s)
             .replace('{{img}}',mydata[s].img)
@@ -26,11 +27,10 @@ function addPhotos(){
     }
     html.push('<div class="nav">'+nav.join('')+'</div>');
     g('#wrap').innerHTML = html.join('');
-    //  rsort(1);
-    rsort(random([-1,data.length-1])); //生成0-7
+    rsort(random([-1,data.length-1])); 
 }
 addPhotos();
-function range(){
+function range(){//计算左右分区的范围
     var range={ left:{ x:[],y:[] },right:{ x:[] ,y:[] }};
     var wrap={
         w: g('#wrap').clientWidth,
@@ -48,9 +48,9 @@ function range(){
     range.right.y=range.left.y;
     return range;
 }
-function rsort(n){
-    var  _photo = g('.photo');//注意：这里得到的_photo是个HTMLCollection，和nodelist一样都是类数组，不是真正的数组
-    var photos=[];//得到一个真正的数组
+function rsort(n){//海报排序
+    var  _photo = g('.photo');
+    var photos=[];
     for(var s=0;s<_photo.length;s++){
         _photo[s].className = _photo[s].className.replace(/\s*photo_center\s*/,' ');
         _photo[s].className = _photo[s].className.replace(/\s*photo_front\s*/,' ');
@@ -63,17 +63,17 @@ function rsort(n){
     }
     var photo_center = g('#photo_'+n);
     photo_center.className += ' photo_center';
-    photo_center = photos.splice(n,1)[0]; //这里赋值给photo_center有必要么？
-    var photos_left = photos.splice(0,Math.ceil(photos.length/2));
+    photo_center = photos.splice(n,1)[0]; 
+    var photos_left = photos.splice(0,Math.ceil(photos.length/2));//海报分为左右两个部分
     var photos_right = photos;
     var ranges = range();
-    for(var s in photos_left){
+    for(var s in photos_left){//左分区
         var photo = photos_left[s];
         photo.style.left= random(ranges.left.x)+'px';
         photo.style.top= random(ranges.left.y)+'px';
         photo.style['transform']='rotate('+random([-150,150])+'deg)';
     }
-    for(var s in photos_right){
+    for(var s in photos_right){//右分区
         var photo = photos_right[s];
         photo.style.left= random(ranges.right.x)+'px';
         photo.style.top= random(ranges.right.y)+'px';
@@ -86,7 +86,7 @@ function rsort(n){
     }
     g('#nav_'+n).className += ' i_current ';
 }
-function turn(elem){
+function turn(elem){//翻转控制
     var str = elem.className;
     var pattern = /photo_front/;
     var n= elem.id.split('_')[1];
